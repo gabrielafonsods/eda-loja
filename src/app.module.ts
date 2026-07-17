4d14b194bc278b423685a5ed4e55518eb69d02dc
Gabriel Afonso dos Santos <162643812+gabrielafonsods@users.noreply.github.com>
2026-07-17 00:00:45 -0300

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { ProductsModule } from './products/products.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      url: 'postgresql://neondb_owner:npg_N1XQmze2bwWT@ep-orange-bar-at9x14ve.c-9.us-east-1.aws.neon.tech/neondb?sslmode=require',
+    
       autoLoadEntities: true,
       synchronize: true,
       ssl: { rejectUnauthorized: false },
